92df64e7b1570bd0f80d4db1c33b5f568e54748b
TJ <tultu@outlook.com>
2018-08-07 22:30:20 +0800

    add new python source file Hello World1.py

---
+++ HelloWorld1.py
@@ -0,0 +1,17 @@
+"""
+This is a groups style docs.
+
+Parameters:
+    param1 - this is the first param
+    param2 - this is a second param
+
+Returns:
+    This is a description of what is returned
+
+Raises:
+    KeyError - raises an exception
+"""
+print('Hello World!')
+YOURNAME = input('Please input your name: ')
+print('Hello', YOURNAME)
+print('Bye!')
